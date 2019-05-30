a4dd7fd272eae1bc4ec00a21ce09efda9ee66e09
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-05-30 10:21:03 -0400

    Add Emacs dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((ess-r-mode . ((ess-r-package-dirs . (("R" . 1) ("tests" . 1) ("testthat" . 2)
+                                       ("analysis" . 1) ("scripts" . 2)))))
+ (nil . ((projectile-project-compilation-cmd . "make"))))
