39bb378831f78a78d085e1824ebd87dafe328804
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-05-30 10:15:29 -0400

    Add flag to control if all draws are run

--- analysis/drake.R
+++ analysis/drake.R
@@ -16,7 +16,9 @@ expose_imports("hector.permafrost.emit")
 # To make this reproducible
 draws <- read_csv(file.path("analysis", "data",
                             "derived_data", "parameter-draws.csv"))
-draws <- head(draws, 100)
+if (!("--all" %in% commandArgs(trailingOnly = TRUE))) {
+  draws <- head(draws, 100)
+}
 params <- rlang::syms(colnames(draws))
 
 plan <- drake_plan(
@@ -100,26 +102,27 @@ plan <- drake_plan(
   )
 )
 
-dconf <- drake_config(
-  plan,
-  parallelism = "future",
-  jobs = parallel::detectCores(),
-  prework = quote({
-    devtools::load_all(".", quiet = TRUE)
-  })
-)
-
-# Set number of cores depending on number of outdated tasks
-dout <- outdated(dconf)
-if (length(dout) > 10) {
-  dconf[["jobs"]] <- parallel::detectCores()
-  message("Number of outdated targets (", length(dout), ") ",
-          "is greater than 10.",
-          "Running in parallel across ", dconf[["jobs"]], " cores.")
-}
-
 if (interactive()) {
+  ## callr::rscript(here::here("analysis", "drake.R"), cmdargs = "--all")
   callr::rscript(here::here("analysis", "drake.R"))
 } else {
+  dconf <- drake_config(
+    plan,
+    parallelism = "future",
+    jobs = parallel::detectCores(),
+    prework = quote({
+      devtools::load_all(".", quiet = TRUE)
+    })
+  )
+
+  # Set number of cores depending on number of outdated tasks
+  dout <- outdated(dconf)
+  if (length(dout) > 10) {
+    dconf[["jobs"]] <- parallel::detectCores()
+    message("Number of outdated targets (", length(dout), ") ",
+            "is greater than 10.",
+            "Running in parallel across ", dconf[["jobs"]], " cores.")
+  }
+
   make(config = dconf)
 }
