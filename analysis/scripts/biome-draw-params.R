513a0b673d27e0577e6fb3189b4d8864ba19f829
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-08-09 13:31:09 -0400

    Update biome-draw-params to use new system

--- analysis/scripts/biome-draw-params.R
+++ analysis/scripts/biome-draw-params.R
@@ -10,32 +10,34 @@ logdir <- here("logs")
 dir.create(logdir, recursive = TRUE, showWarnings = FALSE)
 outdir <- here("analysis", "data", "output")
 dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
+datadir <- here("analysis", "data", "derived_data")
+dir.create(datadir)
 
 set.seed(8675309)
 ndraws <- 10000
 
 npp_alpha <- c(f_nppv = 0.35, f_nppd = 0.60, f_npps = 0.05)
 gl_npp_draws <- rdirichlet(ndraws, npp_alpha)[, 1:2] %>%
-  `colnames<-`(., paste0("global.", colnames(.)))
+  `colnames<-`(., paste0("default.", colnames(.)))
 pf_npp_draws <- rdirichlet(ndraws, npp_alpha)[, 1:2] %>%
   `colnames<-`(., paste0("permafrost.", colnames(.)))
 
 draws <- tibble(
-  global.beta = runif(ndraws, 0, 1),
-  global.q10_rh = runif(ndraws, 0, 10),
-  global.f_litterd = rbeta(ndraws, 0.98 * 4, 0.02 * 4),
+  default.beta = runif(ndraws, 0, 1),
+  default.q10_rh = runif(ndraws, 0, 10),
+  default.f_litterd = rbeta(ndraws, 0.98 * 4, 0.02 * 4),
   permafrost.beta = runif(ndraws, 0, 1),
   permafrost.q10_rh = runif(ndraws, 0, 10),
   permafrost.f_litterd = rbeta(ndraws, 0.98 * 4, 0.02 * 4),
-  frac_veg = rbeta(ndraws, 1, 2),
-  frac_soil = rbeta(ndraws, 1.1, 1.1),
-  frac_detritus = rbeta(ndraws, 1.1, 1.1),
-  warmingfactor = 1 + 3 * rbeta(ndraws, 5, 5)
+  fveg_c = rbeta(ndraws, 1, 2),
+  fsoil_c = rbeta(ndraws, 1.1, 1.1),
+  fdetritus_c = rbeta(ndraws, 1.1, 1.1),
+  ## fnpp_flux0 = rbeta(ndraws, 1.1, 1.1),
+  permafrost.warmingfactor = 1 + 3 * rbeta(ndraws, 5, 5)
 ) %>%
   bind_cols(as_tibble(gl_npp_draws), as_tibble(pf_npp_draws))
 
-write_csv(draws, here("analysis", "data",
-                      "derived_data", "biome-parameter-draws.csv"))
+write_csv(draws, file.path(datadir, "biome-parameter-draws.csv"))
 
 hector_fun <- function(...) {
   library(hector.permafrost.emit)
