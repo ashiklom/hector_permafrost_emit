8e10b0064afd93e7e476580c2db4b8fbe498be92
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2020-02-06 15:28:56 -0500

    Tweak methane calibration script

--- analysis/scratch/methane-calibrate.R
+++ analysis/scratch/methane-calibrate.R
@@ -18,17 +18,19 @@ invisible(reset(hc))
 invisible(run(hc))
 results_methane <- fetchvars(hc, yrs, outvars_both, scenario = "methane")
 results_both <- as_tibble(bind_rows(results_default, results_methane))
-ggplot(results_both) +
+p1 <- ggplot(results_both) +
   aes(x = year, y = value, color = scenario) +
   geom_line() +
   geom_vline(xintercept = 2100, linetype = "dashed") +
   facet_wrap(vars(variable), scales = "free")
 
-ggsave("analysis/figures/dynamic-methane.png")
+## ggsave("analysis/figures/dynamic-methane.png")
 
 # Put it together with the permafrost model
 outvars_both2 <- c(outvars_ch4, outvars_co2, GLOBAL_TEMP(),
-                  ATMOSPHERIC_CO2(), GLOBAL_TEMP(), F_FROZEN(), SOIL_C())
+                   ATMOSPHERIC_CO2(), GLOBAL_TEMP(), F_FROZEN(), SOIL_C(),
+                   RH(), RH_SOIL(), RH_DETRITUS(),
+                   VEG_C(), DETRITUS_C(), SOIL_C())
 yrs <- 1750:2300
 hc <- newcore(rcp45)
 invisible(run(hc))
@@ -41,12 +43,13 @@ invisible(run(hc))
 results_pf <- fetchvars(hc, yrs, outvars_both2, scenario = "permafrost")
 results_allpf <- bind_rows(results_nopf, results_pf) %>%
   as_tibble()
-
-ggplot(results_allpf) +
+p2 <- ggplot(results_allpf) +
   aes(x = year, y = value, color = scenario) +
   geom_line() +
-  geom_vline(xintercept = 2100, linetype = "dashed") +
+  ## geom_vline(xintercept = 2100, linetype = "dashed") +
   facet_wrap(vars(variable), scales = "free") +
   ggtitle("RCP 4.5")
+## p2 %+% filter(results_allpf, year < 1900)
+p2
 
-ggsave("analysis/figures/pf-methane-rcp45.png")
+## ggsave("analysis/figures/pf-methane-rcp45.png")
